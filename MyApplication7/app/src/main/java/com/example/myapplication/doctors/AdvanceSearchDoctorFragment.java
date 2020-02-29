ef1fd8dacc0cef0178d8b4adf300717c03f3b9d4
Akram <Ak.bensalem@esi-sba.dz>
2020-02-29 12:52:20 +0100

    i change search advanced XML

--- MyApplication7/app/src/main/java/com/example/myapplication/doctors/AdvanceSearchDoctorFragment.java
+++ MyApplication7/app/src/main/java/com/example/myapplication/doctors/AdvanceSearchDoctorFragment.java
@@ -4,6 +4,8 @@ package com.example.myapplication.doctors;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -11,6 +13,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.myapplication.R;
+
+import java.util.ArrayList;
 
 
 /**
@@ -27,10 +31,36 @@ public class AdvanceSearchDoctorFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        Log.d("akram","we stil alive 20");
+        Log.d("akram","we stil alive advanceSearchFragment 1");
+        View view = inflater.inflate(R.layout.fragment_doctor_advance_search, container, false);
+        RecyclerView mRecyclerView =  view.findViewById(R.id.doctor_recycler_advanced_search);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        ArrayList<Doctors> mDoctorsData = new ArrayList<Doctors>();
+        Log.d("akram","we stil alive fragment advanceSearchFragment 5");
+
+        String[] NameDoctors = getResources().getStringArray(R.array.doctor_names);
+        Log.d("akram","we stil alive advanceSearchFragment 6");
+
+        String[] PlaceDoctors = getResources().getStringArray(R.array.doctor_place);
+        Log.d("akram","we stil alive advanceSearchFragment 7");
+
+        String[] SexDactors = getResources().getStringArray(R.array.doctor_sex);
+        Log.d("akram","we stil alive advanceSearchFragment 8");
+
+        for (int i = 0; i < NameDoctors.length; i++) {
+            mDoctorsData.add(new Doctors(NameDoctors[i], PlaceDoctors[i], SexDactors[i]));
+        }
+        Log.d("akram","we stil alive advanceSearchFragment 9");
+
+        DoctorsAdapter mAdapter= new DoctorsAdapter( getActivity(), mDoctorsData);
+        Log.d("akram","we stil alive advanceSearchFragment 10");
+
+        mRecyclerView.setAdapter(mAdapter);
+        Log.d("akram","we stil alive advanceSearchFragment 11");
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_doctor_advance_search, container, false);
+        return view;
     }
 
 }
